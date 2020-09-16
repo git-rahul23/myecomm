86e682428cb3f0b47072536354078948d441cd4a
git-rahul23 <jr.rahul07@gmail.com>
2020-09-16 19:10:02 +0530

    Updates

---
+++ precache-manifest.cef56fa5a11df40c8ab9c9dc0ae1428a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38136092492110ba971a8ceed6724e29",
+    "url": "/myecomm/index.html"
+  },
+  {
+    "revision": "2a4c3c2a6c153b02608a",
+    "url": "/myecomm/static/css/main.f7d9ebb7.chunk.css"
+  },
+  {
+    "revision": "79d3703073448932d086",
+    "url": "/myecomm/static/js/2.3e31f7e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myecomm/static/js/2.3e31f7e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4c3c2a6c153b02608a",
+    "url": "/myecomm/static/js/main.8b831554.chunk.js"
+  },
+  {
+    "revision": "d6f021a29a986fbd9319",
+    "url": "/myecomm/static/js/runtime-main.38765ae6.js"
+  },
+  {
+    "revision": "01d6996436c103d471577aed3c0445c1",
+    "url": "/myecomm/static/media/Group 24.01d69964.png"
+  },
+  {
+    "revision": "ee0c4bffe5b48da5918e8b3579a07f51",
+    "url": "/myecomm/static/media/listfive.ee0c4bff.PNG"
+  },
+  {
+    "revision": "fd1228769df6a7562059fef2a935de54",
+    "url": "/myecomm/static/media/listfour.fd122876.PNG"
+  },
+  {
+    "revision": "586020c08611d048be51537964cc4fba",
+    "url": "/myecomm/static/media/listone.586020c0.PNG"
+  },
+  {
+    "revision": "e92e1c2870993076223dd4b4664a1215",
+    "url": "/myecomm/static/media/listsix.e92e1c28.PNG"
+  },
+  {
+    "revision": "3c929e068fdbffb7d70536ef619fcfaf",
+    "url": "/myecomm/static/media/listthree.3c929e06.PNG"
+  },
+  {
+    "revision": "291cae5c7274c593918805abada3b17c",
+    "url": "/myecomm/static/media/listtwo.291cae5c.PNG"
+  },
+  {
+    "revision": "d8c28b13332df198d7d9c1c0b5841e5d",
+    "url": "/myecomm/static/media/picfive.d8c28b13.PNG"
+  },
+  {
+    "revision": "106eabbaf8c456fbee9c6b523e7bed22",
+    "url": "/myecomm/static/media/picfour.106eabba.PNG"
+  },
+  {
+    "revision": "ae2ac7104c5f1bd60b82f1e383413f05",
+    "url": "/myecomm/static/media/picone.ae2ac710.PNG"
+  },
+  {
+    "revision": "2423b0668d6d1c17ae180867679074a2",
+    "url": "/myecomm/static/media/picsix.2423b066.PNG"
+  },
+  {
+    "revision": "c649feacfb069a4e2c98659a3b343304",
+    "url": "/myecomm/static/media/picthree.c649feac.PNG"
+  },
+  {
+    "revision": "d8bb9d2acf190a3013c1d291b007c611",
+    "url": "/myecomm/static/media/pictwo.d8bb9d2a.PNG"
+  }
+]);
